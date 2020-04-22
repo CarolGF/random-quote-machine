6fe747b7fe46dd8df10e8a5b8a2258aedee36062
CarolGF <karola_gf@hotmail.com>
2020-04-22 15:36:37 +0100

    Updates

---
+++ precache-manifest.78ae7f3784d49540841031153eb2859e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41c10dcebe9570ee2e9b07dac37d9c1",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "53fe2308fcf2510afebc",
+    "url": "/random-quote-machine/static/css/main.9d990e50.chunk.css"
+  },
+  {
+    "revision": "9320ae19d2f753310b84",
+    "url": "/random-quote-machine/static/js/2.8d2d9286.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/random-quote-machine/static/js/2.8d2d9286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fe2308fcf2510afebc",
+    "url": "/random-quote-machine/static/js/main.50ec2ae1.chunk.js"
+  },
+  {
+    "revision": "52b3aacfd4b50869dda1",
+    "url": "/random-quote-machine/static/js/runtime-main.116f0ca4.js"
+  }
+]);
